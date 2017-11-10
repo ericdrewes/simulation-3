c16fcfde6f550e631e5b427dd08f4fcc671b818d
valentina abramova <valentina@v.local>
2017-11-10 17:16:34 -0600

    blah

--- simulation-3/src/router.js
+++ simulation-3/src/router.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage.js';
 import store from './store';
 import { Provider } from 'react-redux';
+import Auth from './components/Auth';
 export default (
 
 
@@ -11,6 +12,7 @@ export default (
 	<Provider store={store}>
 		<Switch>
 			<Route exact path="/" component={LandingPage} />
+			<Route path ='/auth' component = {Auth} />
 		</Switch>
 	</Provider>
 
